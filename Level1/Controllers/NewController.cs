e1ed1e768e554fbabccd0e399317a8017e7198c7
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:13:18 +0530

    files added

---
+++ Level1/Controllers/NewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level1.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Create a web API controller with CRUD operations.
+           // a.Create a new controller
+           //b.Inject DbContext and test the controller by adding read functionality.
+
+
+            return View();
+        }
+    }
+}
